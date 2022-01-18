62c0ba18208bc6ed52c9ac0a17e3444d8fcaa388
Jonathan Ramos <jonathanramos@gmail.com>
2022-01-18 10:16:29 -0600

    Change Monday Item Id type from int to string.
    - Fix Item Id type references.

--- Monday.Client/Responses/CreateItemResponse.cs
+++ Monday.Client/Responses/CreateItemResponse.cs
@@ -3,8 +3,14 @@ using Newtonsoft.Json;
 
 namespace Monday.Client.Responses
 {
+    /// <summary>
+    /// Describes the Monday response for a new Item request.
+    /// </summary>
     public class CreateItemResponse
     {
+        /// <summary>
+        /// The item created.
+        /// </summary>
         [JsonProperty("create_item")] public Item Item { get; set; }
     }
 }
